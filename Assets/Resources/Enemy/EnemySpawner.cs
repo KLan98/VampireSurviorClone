f58f4352ccda52f2a31eec6199932a1941942fea
Lan Pham <49123155+KLan98@users.noreply.github.com>
2025-08-05 12:02:37 +0200

    Added fields for enemy spawner

--- Assets/Resources/Enemy/EnemySpawner.cs
+++ Assets/Resources/Enemy/EnemySpawner.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject[] enemyPrefabs;
+    [SerializeField] private int poolSize = 500;
+    [SerializeField] private int enemiesSpawned = 0;
+    [SerializeField] private float spawnInterval = 1.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //private Vector2 GetSpawnPosition()
+    //{
+    //    return new Vector2(Ran)
+    //}
 }
